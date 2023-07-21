01f6e0d183c708a6ba1c5062d21c71858b9b9939
Shitanshu Shivam <74671269+shitan198u@users.noreply.github.com>
2023-07-21 14:01:25 +0530

    Create WhichReportToEachEmployee.sql

---
+++ WhichReportToEachEmployee.sql
@@ -0,0 +1,10 @@
+# Write your MySQL query statement below
+SELECT e1.employee_id, e1.name, 
+COUNT(e2.employee_id) AS reports_count, 
+ROUND(AVG(e2.age)) AS average_age
+FROM Employees e1
+LEFT JOIN Employees e2 
+ON e1.employee_id = e2.reports_to
+GROUP BY e1.employee_id 
+HAVING average_age IS NOT NULL
+ORDER BY employee_id;
